2426a09273ef1c94c6a72c308ab6221586e7fc72
zhangjun-1 <63394849+zhangjun-1@users.noreply.github.com>
2022-06-09 17:27:29 +0800

    Update 抓取屏幕并生成GIF.py

--- 抓取屏幕并生成GIF.py
+++ 抓取屏幕并生成GIF.py
@@ -4,15 +4,17 @@ from PIL import ImageGrab,Image
 m = int(input("输入抓取时间（min）："))
 m = m * 60
 n = 1
+#循环生成图片
 while n < m:
     sleep(0.02)
     im = ImageGrab.grab()
     local = (r"%s.jpg" % (n))
     im.save(local,'jpeg')
     n = n + 1
+#读取生成的图片并排序
 listjpg = [x for x in os.listdir() if re.search(r'jpg$',x)]
 listjpg.sort(key= lambda x:int(x.split('.')[0]))
-
+#生成gif
 im = Image.open('1.jpg')
 images = []
 for i in listjpg:
