090b15fd77c60622bf7d4159b28004a4d7209b5d
zhangjun-1 <63394849+zhangjun-1@users.noreply.github.com>
2022-06-09 15:19:10 +0800

    Create 彩票35选7.py

---
+++ 彩票35选7.py
@@ -0,0 +1,14 @@
+import random
+
+temp = [ i + 1 for i in range(35)]
+random.shuffle(temp)
+i= 0
+list = []
+while i < 7:
+    list.append(temp[i])
+    i = i + 1
+list.sort()
+print('\033[0;31;;1m')
+print(*list[0:6],end='')
+print('\033[0;34;;1m',end=" ")
+print(list[-1])
